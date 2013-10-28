6d579b8eb39cd4e0f693a207fde990277ea21cad
Maxime Bury <maxime.bury@gmail.com>
2013-10-28 18:37:30 -0400

    Add a deploy recipe

--- servicetown/recipes/deploy.rb
+++ servicetown/recipes/deploy.rb
@@ -7,17 +7,49 @@
 # All rights reserved - Do Not Redistribute
 #
 
-include_recipe "deploy"
-
 node[:deploy].each do |application, deploy|
-  opsworks_deploy_dir do
-    user  deploy[:user]
-    group deploy[:group]
-    path  deploy[:deploy_to]
+  directory ::File.join(deploy[:deploy_to], "shared") do
+    recursive true
+    action :create
   end
 
-  opsworks_deploy do
-    deploy_data deploy
-    app application
+  deploy_revision "#{deploy[:deploy_to]}" do
+    repo     deploy[:scm][:repository]
+    revision deploy[:scm][:revision] || "master"
+
+    symlink_before_migrate.clear
+    create_dirs_before_symlink.clear
+    purge_before_symlink.clear
+    symlinks.clear
+
+    restart_command "sudo service #{application} restart"
+    before_restart do
+      execute "package the application" do
+        cwd release_path
+        user "root"
+        command "play clean stage"
+      end
+
+      # Update the service template
+      template "/etc/init.d/#{application}" do
+        source "service.erb"
+        owner "root"
+        group "root"
+        mode  "0755"
+        variables({
+          :name => application,
+          :path => release_path,
+          :options => "",
+          :command => "target/start"
+        })
+      end
+
+      service application do
+        supports :status => true, :start => true, :stop => true, :restart => true
+        action :enable
+      end
+    end
+
+    action :force_deploy
   end
 end
