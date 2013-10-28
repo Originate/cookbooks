c3884b425abf98caa85c052700cbaf170bd487a5
Maxime Bury <maxime.bury@gmail.com>
2013-10-28 18:36:48 -0400

    Upgrade to artifact 1.10.3

--- servicetown/metadata.rb
+++ servicetown/metadata.rb
@@ -7,4 +7,4 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends "java", "~> 1.14.0"
-depends "artifact", "~> 1.10.1"
+depends "artifact", "~> 1.10.3"
